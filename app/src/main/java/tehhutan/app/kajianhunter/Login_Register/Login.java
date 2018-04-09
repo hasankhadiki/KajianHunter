c0b7b14c9f039239bd2ae463747b641262f3ad85
Amir Mu'tashim Billah <amirmutashim@gmail.com>
2018-04-09 23:26:45 +0700

    Coba 1

---
+++ app/src/main/java/tehhutan/app/kajianhunter/Login_Register/Login.java
@@ -0,0 +1,15 @@
+package tehhutan.app.kajianhunter.Login_Register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tehhutan.app.kajianhunter.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
